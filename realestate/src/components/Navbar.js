61eb37eea6622ef7750c86ef089277f061c48170
tobias <tobias.omondi@student.moringaschool.com>
2024-01-17 14:22:42 +0300

    navbarjs

--- realestate/src/components/Navbar.js
+++ realestate/src/components/Navbar.js
@@ -6,21 +6,22 @@ const Navbar = () => {
   return (
     <nav className='navbar'>
         <div className='nav_home'>
-            <Link to= '/'>Home</Link>
+            <Link to= '/' className='nav_live' style={{ textDecoration: 'none' }}>Home</Link>
         </div>
         <div className='nav_aboutus'>
-          <Link to='/about'>About us</Link>
+          <Link to='/about' className='nav_live'  style={{ textDecoration: 'none' }}>About us</Link>
         </div>
         <div className='nav_comapanies'>
-          <Link to='/companies'>Companies</Link>
+          <Link to='/companies' className='nav_live'  style={{ textDecoration: 'none' }}>Companies</Link>
         </div>
         <div className='nav_agents'>
-          <Link to='/agent'>Agents</Link>
+          <Link to='/agent' className='nav_live'  style={{ textDecoration: 'none' }}>Agents</Link>
         </div>
         <div className='nav_login'>
-          <Link to='/login'>Login</Link>
-        </div>
-      
+        <button className='nav_live_btn'>
+          <Link to='/login' style={{ textDecoration: 'none', color: 'inherit' }}>Login</Link>
+        </button>
+      </div>
     </nav>
   )
 }
