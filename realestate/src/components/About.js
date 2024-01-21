dbaa47e3231c763e04b6d87cfa53b5f8df6191cb
tobias <tobias.omondi@student.moringaschool.com>
2024-01-21 14:39:54 +0300

    updated

--- realestate/src/components/About.js
+++ realestate/src/components/About.js
@@ -1,15 +1,27 @@
 import React from 'react'
 import Footer from './Footer'
+import './About.css'
 
 const About = () => {
   return (
-    <div>
-      <h1>About us</h1>
-      love cbdcdhjijadsfdsdd
+    <div className='about_us'>
+      <div className='about_us_title'>
+        <p>How it Started</p>
+      </div>
+      <div className='about_stories'>
+        
+    <p>In the year 2024, DwellingHub emerged as a beacon of innovation in the realm of real estate. <br/>
+       Conceived by a dedicated group of visionaries, the platform was crafted with a singular focus:<br/>
+       to simplify and elevate the real estate experience. DwellingHub is more than a digital marketplace;<br/>
+       it's a community where seekers of homes and investment opportunities converge. <br/>
+       With an unwavering commitment to transparency and user-friendly navigation, DwellingHub invites you <br/>
+       to explore a world where finding your perfect dwelling is not just a transaction but a journey, <br/>
+       guided by simplicity and enriched by a passion for redefining how we connect with real estate.<br/>
+      Welcome to DwellingHub, where possibilities in the world of real estate unfold at your fingertips.</p>
+    </div>
 
       <div>
         <Footer />
-
       </div>
     </div>
   )
