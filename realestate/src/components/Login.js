6434ac921c6a743110f37dee67833d3781b56d17
tobias <tobias.omondi@student.moringaschool.com>
2024-01-23 21:45:07 +0300

    updated

--- realestate/src/components/Login.js
+++ realestate/src/components/Login.js
@@ -1,15 +1,52 @@
-import React from 'react'
-import Footer from './Footer'
+// LoginForm.js
+import React, { useState } from 'react';
 
-const Login = () => {
+const LoginForm = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add your login logic here, e.g., make an API request to authenticate the user
+    console.log('Form submitted with data:', formData);
+    // You can replace the above console.log with your actual login logic
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
-      <div>
-        <Footer />
-      </div>
-    </div>
-  )
-}
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input
+          type="text"
+          name="username"
+          value={formData.username}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <button type="submit">Login</button>
+    </form>
+  );
+};
 
-export default Login
+export default LoginForm;
