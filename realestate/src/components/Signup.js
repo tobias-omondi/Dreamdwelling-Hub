810bdd783a2e6ed7d9637fed39b2766af320f6a0
tobias <tobias.omondi@student.moringaschool.com>
2024-01-23 21:44:07 +0300

    updated

--- realestate/src/components/Signup.js
+++ realestate/src/components/Signup.js
@@ -1,16 +1,63 @@
-import React from 'react'
-import Footer from './Footer'
+// SignUpForm.js
+import React, { useState } from 'react';
 
-const Signup = () => {
+const SignUpForm = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add your signup logic here, e.g., make an API request to register the user
+    console.log('Form submitted with data:', formData);
+    // You can replace the above console.log with your actual signup logic
+  };
+
   return (
-    <div>
-      <h1>Signup</h1>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input
+          type="text"
+          name="username"
+          value={formData.username}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <label>
+        Email:
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <button type="submit">Sign Up</button>
+    </form>
+  );
+};
 
-      <div>
-        <Footer />
-      </div>
-    </div>
-  )
-}
-
-export default Signup
+export default SignUpForm;
