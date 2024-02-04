f19b1a3e767490ea5e0f0bd295bd36a8d5e4ac4a
tobias <tobias.omondi@student.moringaschool.com>
2024-02-04 12:49:36 +0300

    updated

--- realestate/src/components/Agents.js
+++ realestate/src/components/Agents.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import Footer from './Footer'
+import './Agents.css'
+import Asset7 from './Asset/Premium Photo _ A jet plane flying over the city.jpeg'
 
 const Agents = () => {
   return (
-    <div>
-      <h1>Agents</h1>
-      <div>
+    <div className='Agents_pageee'>
+    <div className='Agents_pagee'>
+    <h1 style={{color:'white'}}>Where Dreams Meet Reality</h1>
+    <p style={{color:'white'}}>DreamDwelling Hub stands as a beacon in the realm of real estate, renowned for its exceptional team of agents who bring dreams to life in the form of cherished homes. These agents, more than mere intermediaries between buyers and sellers, are architects of aspirations, navigators of hopes, and custodians of dreams.
+
+Navigating the intricate landscape of real estate, DreamDwelling Hub's agents possess an unparalleled expertise honed through years of experience and a deep understanding of market dynamics. They are adept at discerning the unique desires and needs of their clients, ensuring that every transaction is tailored to perfection.
+
+Beyond their professional acumen, DreamDwelling Hub's agents are passionate storytellers, weaving narratives that transform mere properties into realms of possibility. They understand that a home is not just a structure of bricks and mortar but a sanctuary where memories are forged and futures take shape.
+
+With an unwavering commitment to excellence, DreamDwelling Hub's agents go above and beyond to exceed expectations. They stand as pillars of support, guiding clients through every step of the journey with integrity, transparency, and a steadfast dedication to achieving the best possible outcomes.
+
+In the bustling world of real estate, where dreams and realities converge, DreamDwelling Hub's agents shine as beacons of trust, reliability, and unwavering dedication to turning dreams into cherished realities.</p>
+      <img src={Asset7} style={{width:'50%',height:'85vh'}} alt='Agents city' />
+      </div>
+        <div>
         <Footer/>
       </div>
     </div>
